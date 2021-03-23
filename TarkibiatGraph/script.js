27186de7929805b61be4ca88888570a6fb0907b9
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-03-23 16:47:45 +0430

    add graph class

---
+++ TarkibiatGraph/script.js
@@ -0,0 +1,47 @@
+class Graph {
+  selectedVertices = [];
+  selectedEdges = [];
+  nodes = [];
+  edges = [];
+
+  constructor(node, edges) {
+    this.nodes = nodes;
+    this.edges = edges;
+  }
+
+  selectVertex(vertexId) {
+
+  }
+
+  unselectVertex(vertexId) {
+
+  }
+
+  selectEdge(edgeId) {
+
+  }
+
+  unselectEdge(edgeId) {
+
+  }
+
+  addVertex() {
+
+  }
+
+  removeSelectedVertices() {
+
+  }
+
+  addEdgesBetweenSelectedVertices() {
+
+  }
+
+  removeEdgesBetweenSelectedVertices() {
+
+  }
+};
+
+class TarkibiatGraph extends Graph {
+
+}
