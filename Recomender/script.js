7298419fa9d4e0a013ce058d00767692cc125be6
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-03-24 20:29:50 +0430

    all done except _reduceMatrixDimension & initialize table

--- Recomender/script.js
+++ Recomender/script.js
@@ -1,45 +1,57 @@
 class RecommenderGame {
-  table = [[]];
-  finalPoints = [[0, 0]]
-  normalizedMatrix = []
+    table = [[]];
+    normalizedMatrix = [[]]
+    reducedMatrix = [[]]
 
-  RecommenderGame() {
-    this._initializeTable();
-    this._normalizeMatrix();
-
-  }
-
-  _initializeTable() {
-    //todo
-  }
-
-  setTableCellValue(row, column, newValue) {
-    this.table[row][column] = newValue;
-  }
-
-  _normalizeMatrix(){
-    for (var i = 0; i < this.table.length; i++) {
-      var sumOfSquares = 0;
-      for (var j = 0; j < this.table[0].length; j++) {
-        sumOfSquares += this.table[i][j] * this.table[i][j];
-      }
-      this.normalizedMatrix[i] = sumOfSquares;
+    RecommenderGame() {
+        this._initializeTable();
+        this._normalizeMatrix();
+        this._reduceMatrixDimension();
     }
-  }
+
+    _initializeTable() {
+        //todo
+    }
+
+    setTableCellValue(row, column, newValue) {
+        this.table[row][column] = newValue;
+    }
+
+    _normalizeMatrix() {
+
+        for (let i = 0; i < this.table.length; i++) {
+            let j;
+            let sumOfSquares = 0;
+
+            for (j = 0; j < this.table[0].length; j++) {
+                sumOfSquares += this.table[i][j] * this.table[i][j];
+            }
+
+            for (j = 0; j < this.table[0].length; j++) {
+                this.normalizedMatrix[i][j] = this.table[i][j] / Math.sqrt(sumOfSquares);
+            }
+        }
+
+    }
+
+    _reduceMatrixDimension() {
+        this.reducedMatrix = druid.Matrix.from(this.normalizedMatrix).to2dArray;
+        //todo
+    }
 
 
 }
 
 
 const a = [
-  [22, 10, 2, 3, 7],
-  [14, 7, 10, 0, 8],
-  [-1, 13, -1, -11, 3],
-  [-3, -2, 13, -2, 4],
-  [9, 8, 1, -2, 4],
-  [9, 1, -7, 5, -1],
-  [2, -6, 6, 5, 1],
-  [4, 5, 0, -2, 2]
+    [22, 10, 2, 3, 7],
+    [14, 7, 10, 0, 8],
+    [-1, 13, -1, -11, 3],
+    [-3, -2, 13, -2, 4],
+    [9, 8, 1, -2, 4],
+    [9, 1, -7, 5, -1],
+    [2, -6, 6, 5, 1],
+    [4, 5, 0, -2, 2]
 ]
 
 // const { u, v, q } = SVDJS.SVD(a)
