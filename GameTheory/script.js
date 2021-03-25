c65485e33f850358e8805abd8c000908493bba45
narges jahromizadeh <nrgs.jahromi@gmail.com>
2021-03-25 13:42:58 +0430

    add some function to GameTheoryMiniGame class

--- GameTheory/script.js
+++ GameTheory/script.js
@@ -1,59 +1,3 @@
-class Node {
-    constructor(id) {
-        this.id = id;
-    }
-}
-
-class Graph {
-    selectedNodes = [];
-    selectedEdges = [];
-    nodes = [];
-    edges = [];
-
-    constructor(node, edges) {
-        this.nodes = nodes;
-        this.edges = edges;
-    }
-
-    selectNode(nodeId) {
-
-    }
-
-    unselectNode(nodeId) {
-
-    }
-
-    selectEdge(edgeId) {
-
-    }
-
-    unselectEdge(edgeId) {
-
-    }
-
-    addNewNode() {
-
-    }
-
-    removeSelectedNodes() {
-
-    }
-
-    addEdge(node1Id, node2Id) {
-
-    }
-
-    addEdgesBetweenSelectedNodes() {
-
-    }
-
-    removeEdgesBetweenSelectedNodes() {
-
-    }
-};
-
-//////////////
-
 // const patient1={
 //   name:'ali',
 //   friendname:'reza',
@@ -72,9 +16,50 @@ class Graph {
 //   list=[],
 // };
 
+const LEVELS_INFO = [{
+        levelNumber: 0,
+        people: [{
+                id: 1,
+                isPatient: true,
+                friendId: 2,
+                priorityList: [4],
+            },
+            {
+                id: 2,
+                isPatient: false,
+                friendId: 1,
+                priorityList: [],
+            },
+            {
+                id: 3,
+                isPatient: true,
+                friendId: 4,
+                priorityList: [2],
+            },
+            {
+                id: 4,
+                isPatient: false,
+                friendId: 3,
+                priorityList: [],
+            },
+        ],
+        answer: [
+            [
+                [0, 2],
+                [2, 3],
+                [3, 0]
+            ],
+            [
+                [1, 5],
+                [5, 4],
+                [4, 6],
+                [6, 1],
+            ],
+        ]
+    },
 
-var str = prompt("inter number of patients:");
-const PEOPLE_NUMBER = Number(str);
+]
+
 const NAMES = [
     'اصغر',
     'محمد',
@@ -87,45 +72,71 @@ const NAMES = [
     'پریوش',
 ];
 
-const friends = [
-    'حشمت',
-    'اشرف',
-    'معصومه',
-    'غلام',
-    'صفرعلی',
-    'بیگُم',
-    'مهین‌تاج',
-    'شهپر',
-    'مهوش',
+const LAST_NAMES = [
+    'فرهادی',
+    'دلربا',
+    'حدادی',
+    'غلام‌رضایی',
+    'کاکایی',
+    'کربکندی',
+    'بی‌طرف',
+    'جعفری',
+    'خان‌پور',
+    'بقال‌زاده',
+    'ماس‌بند',
+    'بقولی‌زاده',
+    'قلی‌پور',
+    'تاتاری',
 ];
 
-
-class Person extends Node {
-    friendId;
-    priorityList = [];
-
-    constructor(priorityList, friendId) {
+class Person {
+    constructor(id, priorityList, friendId, isPatient) {
+        this.id = id;
         this.friendId = friendId;
         this.priorityList = priorityList;
-        this.NAMES = NAMES[Math.floor(Math.random() * NAMES.length)];
-        this.friends = friends[Math.floor(Math.random() * friends.length)];
+        this.isPatient = isPatient;
+        this.name = NAMES[Math.floor(Math.random() * NAMES.length)] + ' ' + LAST_NAMES[Math.floor(Math.random() * LAST_NAMES.length)];
     }
-
 }
 
 
-class GameTheoryMiniGame extends Graph {
-    donorsId = [];
-    patientsId = [];
+class GameTheoryMiniGame {
+    perople = [];
+    edges = [];
 
-    constructor() {
+    constructor(level) {
+        this.LevelInfo = LEVELS_INFO[level];
+        for (let id = 0; id < LevelInfo.people.length; id++) {
+            const initialPerson = LevelInfo.people[id];
+            let newPerson = new Person(id, initialPerson.priorityList, initialPerson.friendId, initialPerson.isPatient);
+            this.perople.push(newPerson);
+        }
+    }
+
+    addEdge(patientId, donorId) {
+        for (let i = 0; i < this.people.length; i++) {
+            if (patientId == i && !this.perople[i].isPatient) {
+                console.log("Error!");
+                return;
+            }
+            if (donorId == i && this.perople[i].isPatient) {
+                console.log("Error!");
+                return;
+            }
+        }
+        this.edges.push([patientId, donorId]);
+        console.log("Edge added succesfully!");
+    }
+
+    removeEdge() {
 
     }
 
-    //override from parent
-    addEdge() {
+    getResult() {
+        const answer = this.LevelInfo.answer;
 
     }
-
-
 }
+
+
+console.log(new GameTheoryMiniGame(0));
