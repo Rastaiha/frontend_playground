2006e9540ea0c3aeb9b0be07730148209769f185
narges jahromizadeh <nrgs.jahromi@gmail.com>
2021-03-26 01:40:08 +0430

    add data of levels

--- GameTheory/script.js
+++ GameTheory/script.js
@@ -1,62 +1,412 @@
-// const patient1={
-//   name:'ali',
-//   friendname:'reza',
-//   list=[],
-// };
-
-// const patient2={
-//   name:'hasan',
-//   friendname:'omid',
-//   list=[],
-// };
-
-// const patient3={
-//   name:'mahdi',
-//   friendname:'reza',
-//   list=[],
-// };
-
-const LEVELS_INFO = [{
+const LEVELS_INFO = [
+    //////////////////////////////////////////////level 0
+    {
         levelNumber: 0,
         people: [{
-                id: 1,
+                id: 0,
                 isPatient: true,
-                friendId: 2,
-                priorityList: [4],
+                friendId: 1,
+                priorityList: [3, 5, 1],
             },
             {
-                id: 2,
+                id: 1,
                 isPatient: false,
-                friendId: 1,
+                friendId: 0,
                 priorityList: [],
             },
             {
-                id: 3,
+                id: 2,
                 isPatient: true,
-                friendId: 4,
-                priorityList: [2],
+                friendId: 3,
+                priorityList: [5, 1, 3],
+            },
+            {
+                id: 3,
+                isPatient: false,
+                friendId: 2,
+                priorityList: [],
             },
             {
                 id: 4,
+                isPatient: true,
+                friendId: 5,
+                priorityList: [5, 7],
+            },
+            {
+                id: 5,
                 isPatient: false,
-                friendId: 3,
+                friendId: 4,
+                priorityList: [],
+            },
+            {
+                id: 6,
+                isPatient: true,
+                friendId: 7,
+                priorityList: [1, 5],
+            },
+            {
+                id: 7,
+                isPatient: false,
+                friendId: 6,
                 priorityList: [],
             },
         ],
         answer: [
+            /// [patient , doner]
             [
-                [0, 2],
-                [2, 3],
-                [3, 0]
+                [0, 3],
+                [2, 5],
+                [4, 7],
+                [6, 1]
             ],
+        ]
+
+
+    },
+    ///////////////////////////////////////////////////////level 1
+    {
+        levelNumber: 1,
+        people: [{
+                id: 0,
+                isPatient: true,
+                friendId: 1,
+                priorityList: [3, 1],
+            },
+            {
+                id: 1,
+                isPatient: false,
+                friendId: 0,
+                priorityList: [],
+            },
+            {
+                id: 2,
+                isPatient: true,
+                friendId: 3,
+                priorityList: [3, 1],
+            },
+            {
+                id: 3,
+                isPatient: false,
+                friendId: 2,
+                priorityList: [],
+            },
+
+        ],
+        answer: [
+            /// [patient , doner]
             [
-                [1, 5],
-                [5, 4],
-                [4, 6],
-                [6, 1],
+                [0, 1],
+                [2, 3]
             ],
         ]
     },
+    ///////////////////////////////////////////////////level 2
+
+    {
+        levelNumber: 2,
+        people: [{
+                id: 0,
+                isPatient: true,
+                friendId: 1,
+                priorityList: [1, 5],
+            },
+            {
+                id: 1,
+                isPatient: false,
+                friendId: 0,
+                priorityList: [],
+            },
+            {
+                id: 2,
+                isPatient: true,
+                friendId: 3,
+                priorityList: [7, 5],
+            },
+            {
+                id: 3,
+                isPatient: false,
+                friendId: 2,
+                priorityList: [],
+            },
+            {
+                id: 4,
+                isPatient: true,
+                friendId: 5,
+                priorityList: [3, 7],
+            },
+            {
+                id: 5,
+                isPatient: false,
+                friendId: 4,
+                priorityList: [],
+            },
+            {
+                id: 6,
+                isPatient: true,
+                friendId: 7,
+                priorityList: [3, 5],
+            },
+            {
+                id: 7,
+                isPatient: false,
+                friendId: 6,
+                priorityList: [],
+            },
+
+        ],
+        answer: [
+            /// [patient , doner]
+            [
+                [0, 1]
+            ],
+            [
+                [2, 7],
+                [6, 3]
+            ],
+            [
+                [4, 5]
+            ],
+        ]
+    },
+    ///////////////////////////////////////////////////level 3
+
+    {
+        levelNumber: 3,
+        people: [{
+                id: 0,
+                isPatient: true,
+                friendId: 1,
+                priorityList: [1, 5],
+            },
+            {
+                id: 1,
+                isPatient: false,
+                friendId: 0,
+                priorityList: [],
+            },
+            {
+                id: 2,
+                isPatient: true,
+                friendId: 3,
+                priorityList: [1, 5],
+            },
+            {
+                id: 3,
+                isPatient: false,
+                friendId: 2,
+                priorityList: [],
+            },
+            {
+                id: 4,
+                isPatient: true,
+                friendId: 5,
+                priorityList: [3, 7],
+            },
+            {
+                id: 5,
+                isPatient: false,
+                friendId: 4,
+                priorityList: [],
+            },
+            {
+                id: 6,
+                isPatient: true,
+                friendId: 7,
+                priorityList: [5, 9],
+            },
+            {
+                id: 7,
+                isPatient: false,
+                friendId: 6,
+                priorityList: [],
+            },
+            {
+                id: 8,
+                isPatient: true,
+                friendId: 9,
+                priorityList: [11, 9],
+            },
+            {
+                id: 9,
+                isPatient: false,
+                friendId: 8,
+                priorityList: [],
+            },
+            {
+                id: 10,
+                isPatient: true,
+                friendId: 11,
+                priorityList: [5, 7],
+            },
+            {
+                id: 11,
+                isPatient: false,
+                friendId: 10,
+                priorityList: [],
+            },
+
+        ],
+        answer: [
+            /// [patient , doner]
+            ///this part is not complete
+            [
+                [],
+                [],
+                []
+            ],
+            [
+                [],
+                [],
+                [],
+                [],
+            ],
+        ]
+    },
+
+    ///////////////////////////////////////////////////level 4
+
+    {
+        levelNumber: 4,
+        people: [{
+                id: 0,
+                isPatient: true,
+                friendId: 1,
+                priorityList: [1, 3],
+            },
+            {
+                id: 1,
+                isPatient: false,
+                friendId: 0,
+                priorityList: [],
+            },
+            {
+                id: 2,
+                isPatient: true,
+                friendId: 3,
+                priorityList: [1, 5],
+            },
+            {
+                id: 3,
+                isPatient: false,
+                friendId: 2,
+                priorityList: [],
+            },
+            {
+                id: 4,
+                isPatient: true,
+                friendId: 5,
+                priorityList: [3, 5],
+            },
+            {
+                id: 5,
+                isPatient: false,
+                friendId: 4,
+                priorityList: [],
+            },
+            {
+                id: 6,
+                isPatient: true,
+                friendId: 7,
+                priorityList: [5, 11, 7, 9],
+            },
+            {
+                id: 7,
+                isPatient: false,
+                friendId: 6,
+                priorityList: [],
+            },
+            {
+                id: 8,
+                isPatient: true,
+                friendId: 9,
+                priorityList: [7, 11],
+            },
+            {
+                id: 9,
+                isPatient: false,
+                friendId: 8,
+                priorityList: [],
+            },
+            {
+                id: 10,
+                isPatient: true,
+                friendId: 11,
+                priorityList: [9],
+            },
+            {
+                id: 11,
+                isPatient: false,
+                friendId: 10,
+                priorityList: [],
+            },
+            {
+                id: 12,
+                isPatient: true,
+                friendId: 13,
+                priorityList: [7, 13, 19],
+            },
+            {
+                id: 13,
+                isPatient: false,
+                friendId: 12,
+                priorityList: [],
+            },
+            {
+                id: 14,
+                isPatient: true,
+                friendId: 15,
+                priorityList: [17, 19, 15],
+            },
+            {
+                id: 15,
+                isPatient: false,
+                friendId: 14,
+                priorityList: [],
+            },
+            {
+                id: 16,
+                isPatient: true,
+                friendId: 17,
+                priorityList: [13, 7, 19],
+            },
+            {
+                id: 17,
+                isPatient: false,
+                friendId: 16,
+                priorityList: [],
+            },
+            {
+                id: 18,
+                isPatient: true,
+                friendId: 19,
+                priorityList: [5, 1, 13, 17],
+            },
+            {
+                id: 19,
+                isPatient: false,
+                friendId: 18,
+                priorityList: [],
+            },
+        ],
+        answer: [
+            /// [patient , doner]
+            ///this part is not complete
+            [
+                [, ],
+                [, ],
+                [, ]
+            ],
+            [
+                [, ],
+                [, ],
+                [, ],
+                [, ],
+            ],
+        ]
+    },
+
+
+
 
 ]
 
